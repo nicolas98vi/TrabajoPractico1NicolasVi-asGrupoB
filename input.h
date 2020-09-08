48415ade66ff869ffb66debda26f01682503ccb2
nicolas98vi <60698088+nicolas98vi@users.noreply.github.com>
2020-09-08 18:45:18 -0300

    Add files via upload

---
+++ input.h
@@ -0,0 +1,12 @@
+
+
+
+int calculoSuma(int primerNumero, int segundoNumero);
+
+int calculoResta(int primerNumero, int segundoNumero);
+
+int calculodivicion(int primerNumero, int segundoNumero);
+
+int calculoMultiplicacion(int primerNumero, int segundoNumero);
+
+int calculoFactorial(int primerNumero);
